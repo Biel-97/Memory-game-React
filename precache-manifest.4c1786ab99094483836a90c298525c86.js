3ca9d6a38999b565d4f3533ac8ee6e5028accc4c
Biel-97 <gabriel.p1997@yahoo.com>
2020-10-23 11:36:19 -0300

    Updates

---
+++ precache-manifest.4c1786ab99094483836a90c298525c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3d5d7d95dd40229d5904337aa8de1b",
+    "url": "/Memory-game-React/index.html"
+  },
+  {
+    "revision": "b917e1e28fdaefa80e83",
+    "url": "/Memory-game-React/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "0e61ee6b5d4d528b494d",
+    "url": "/Memory-game-React/static/css/main.de9625c5.chunk.css"
+  },
+  {
+    "revision": "b917e1e28fdaefa80e83",
+    "url": "/Memory-game-React/static/js/2.9dac0542.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-game-React/static/js/2.9dac0542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e61ee6b5d4d528b494d",
+    "url": "/Memory-game-React/static/js/main.6e64e99b.chunk.js"
+  },
+  {
+    "revision": "9703973d3c50b28ca2b8",
+    "url": "/Memory-game-React/static/js/runtime-main.e472fa36.js"
+  }
+]);
